917fbbf109204a6a93804ae49e5781ad327b216b
Rob Campbell <git@raacampbell.com>
2019-06-03 17:33:08 +0100

    Tidy

--- matlab/@diffusersensor/calcZernike.m
+++ matlab/@diffusersensor/calcZernike.m
@@ -6,7 +6,7 @@ function calcZernike(obj)
     obj.phaseImage=obj.phaseImage-mean(obj.phaseImage(:)); %Get rid of most of the piston
     n=12; % how many coefs to return    
 
-    [obj.zernCoefs,obj.zernNames]=zernike_coeffs(imresize(obj.phaseImage,[obj.zernImSize, nan]),n,obj.printZernCoefs);
+    [obj.zernCoefs,obj.zernNames]=dws.zernike_coeffs(imresize(obj.phaseImage,[obj.zernImSize, nan]),n,obj.printZernCoefs);
     obj.zernCoefs(1)=0; %Force to zero since we can't measure it
 
 end
