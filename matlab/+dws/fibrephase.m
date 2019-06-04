027a59ad6b4a204e087047c26022f5d872166aa1
Rob Campbell <git@raacampbell.com>
2019-06-04 16:08:20 +0100

    add function for calculating theoretical phase over the pupil

---
+++ matlab/+dws/fibrephase.m
@@ -0,0 +1,127 @@
+function varargout=fibrephase(fDist,pupilSize,lambda)
+    % Determine the phase delay of a spherical wave hitting a pupil from a known distance
+    %
+    % out = dw.fibrephase(fDist,pupilSize,lambda)
+    %
+    % Purpose
+    % This function is intended to be used to calculate the phase delay across the sensor
+    % when illuminated by a single-mode fibre at a known distance. 
+    %
+    %
+    % Inputs
+    % fDist - distance between fibre tip and sensor in meters. Default is 1 m. Can also be a
+    %         vector, in which case the delay for multiple distances is evaluated.
+    % pupilSize - the size of the chip in mm along the shortest dimension. Default is 10 mm
+    % lambda - the wavelength of the illumination light in nm. Default is 635 nm.
+    %
+    %
+    % Outputs
+    % out - A structure with the phase curves and inputs used to produce them. 2D phase plots
+    %       are returned. 
+    %
+    %
+    % Examples
+    % >> dws.fibrephase
+    % >> dws.fibrephase(1:0.025:2)
+    %
+    %
+    % Rob Campbell - SWC 2019
+
+
+
+    if nargin<1 || isempty(fDist)
+        fDist=1;
+    end
+    fDist = sort(fDist,'descend');
+
+    if nargin<2 || isempty(pupilSize)
+        pupilSize = 10;
+    end
+
+    if nargin<3 || isempty(lambda)
+        lambda = 635;
+    end
+
+
+    X=[];
+    Y=[];
+    Z=[];
+    for ii=1:length(fDist)
+        % The half-angle subtended by the pupil
+        theta = tan((1E-3*pupilSize/2)/fDist(ii));
+
+        % Create a meshgrid of the points we will evaluate
+        pts=linspace(-theta,theta,256);
+        [xg,yg] = meshgrid(pts,pts);
+
+        [z,x,y]=sph2cart(xg,yg,fDist(ii));
+
+        X = cat(3,X,x);
+        Y = cat(3,Y,y);
+        Z = cat(3,Z,z);
+    end
+
+    tFig=dws.focusNamedFig('fibrephase');
+    clf(tFig)
+
+    % Convert coordinates to mm
+    X=X*1E3;
+    Y=Y*1E3;
+
+    % Turn Z into a phase delay
+    Z=bsxfun(@minus, Z, max(max(Z)) ); %To remove piston
+    Z=(Z*1E9)/lambda;
+
+
+    subplot(1,2,1)
+    doLinePlot(X,Z)
+    ylabel('Phase delay')
+
+    subplot(1,2,2)
+    if length(fDist)>2
+        doLinePlot(X,  bsxfun(@minus,Z,Z(:,:,1)) )
+        ylabel('\Delta Phase delay')
+    else
+        if length(fDist)==2
+            zz = diff(Z,[],3);
+        else
+            zz = Z;            
+        end
+        imagesc(zz)
+        axis square
+        colorbar
+    end    
+
+
+
+    % Prepare output arguments
+    if nargout>0
+        out.sensorPosX = X;
+        out.sensorPosY = Y;
+        out.phaseDelay = Z;
+        out.lambda = lambda;
+        out.pupilSize = pupilSize;
+        out.fDist = fDist;
+        varargout{1}=out;
+    end
+
+
+
+
+
+    % Internal functions follow
+    function doLinePlot(X,Z)
+        n=size(X,3); % The number of distances to plot
+        if n>3
+            set(gca,'ColorOrder',parula(n),'NextPlot', 'replacechildren');
+        else
+            set(gca,'ColorOrder',jet(n),'NextPlot', 'replacechildren');
+        end
+        midPoint = round(size(X,1)/2);
+        xp = squeeze(X(1,:,:));
+        zp = squeeze(Z(midPoint,:,:));
+        plot(xp,zp,'-')
+        axis tight
+        box on
+        grid on
+        xlabel('Position on sensor (mm)')
